6d60acda359f58b0bebdfa9e7509101de82db1db
amahboub <amahboub@c63750b6-0a05-0410-b89b-bf6a48017dd0>
2005-09-23 11:59:56 +0000

    *** empty log message ***
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/cadcoq@34 c63750b6-0a05-0410-b89b-bf6a48017dd0

---
+++ trunk/CAD_COQ/QArith/QArith.v
@@ -0,0 +1,7 @@
+Unset Boxed Definitions.
+Unset Boxed Values.
+
+
+Require Export QArith_base.
+Require Export Qring.
+Require Export Qreduction.
---
+++ trunk/CAD_COQ/QArith/Qring.v
@@ -0,0 +1,76 @@
+
+Unset Boxed Definitions.
+Unset Boxed Values.
+
+
+
+Require Import Ring.
+Require Export Setoid_ring.
+Require Export QArith_base.
+
+Definition Qeq_bool (x y : Q) :=
+  if Qeq_dec x y then true else false.
+
+Lemma Qeq_bool_correct : forall x y : Q, Qeq_bool x y = true -> x==y.
+intros x y; unfold Qeq_bool in |- *; case (Qeq_dec x y); simpl in |- *; auto.
+intros _ H; inversion H.
+Qed.
+
+Definition Qone := 1#1.
+Definition Qzero := 0#1.
+
+Definition Qsrt : Setoid_Ring_Theory Qeq Qplus Qmult Qone Qzero Qopp Qeq_bool.
+Proof.
+constructor.
+exact Qplus_sym.
+exact Qplus_assoc.
+exact Qmult_sym.
+exact Qmult_assoc.
+intro; setoid_rewrite (Qplus_sym Qzero n); apply Qzero_right.
+intro; setoid_rewrite (Qmult_sym Qone n); apply Qmult_n_1.
+exact Qplus_inverse_r.
+intros; setoid_rewrite (Qmult_sym (Qplus n m) p);
+ setoid_rewrite (Qmult_sym n p); setoid_rewrite (Qmult_sym m p);
+ apply Qmult_plus_distr_r; auto.
+unfold Is_true in |- *; intros x y; generalize (Qeq_bool_correct x y);
+ case (Qeq_bool x y); auto.
+Qed.
+
+Add Setoid Ring Q Qeq Q_Setoid Qplus Qmult Qone Qzero Qopp Qeq_bool
+ Qplus_comp Qmult_comp Qopp_comp Qsrt
+ [ Qmake (*inject_Z*)  Zpos 0%Z Zneg xI xO 1%positive ].
+
+(* Exemple of use: *)
+
+Goal
+forall x y z : Q, (x+y)*z ==  (x*z)+(y*z).
+intros.
+ring.
+Abort.
+
+Goal forall x y : Q, x+y == y+x.
+intros. 
+ring.
+Abort.
+
+Goal forall x y z : Q, (x+y)+z == x+(y+z).
+intros.
+ring.
+Abort.
+
+Goal (inject_Z 1)+(inject_Z 1)==(inject_Z 2).
+ring.
+Abort.
+
+Goal  Qone+Qone ==2#1.
+ring.
+Abort.
+
+Goal 1#1+1#1 == 2#1.
+ring.
+Abort.
+
+Goal forall x : Q, x-x== 0#1.
+intro.
+ring.
+Abort.
