cffd7e16866f11d8e4d56ebc53506e8cf227302d
bertot <bertot@c63750b6-0a05-0410-b89b-bf6a48017dd0>
2006-08-18 15:57:05 +0000

    in the middle of proofs about Pol_eval
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/cadcoq@68 c63750b6-0a05-0410-b89b-bf6a48017dd0

--- trunk/CAD_COQ/SmallDescartesLaw.v
+++ trunk/CAD_COQ/SmallDescartesLaw.v
@@ -2,38 +2,39 @@ Require Import Setoid.
 Require Import ZArith.
 Require Import CAD_types.
 Require Import Utils.
-Require Import QArith.
-Require Import Qring.
 Require Import NArith.
 
+Require Import QArith.
+Require Import Qring.
 Require Import Qring.
 Require Import Pol_ring2.
 Import Qnorm.
 
-Open Scope Q_scope.
+Opaque Coef_record.Czero_test Coef_record.C0 Coef_record.Ceq Coef_record.C1
+       Coef_record.Cadd Coef_record.Csub Coef_record.Cmul Coef_record.Cpow.
 
 (* First prove that polynomials with no alternations and at least one
    positive coefficient are increasing and have a limit to infinity at
    infinity. *)
 
-Fixpoint least_non_zero_coeff (p:Pol) : Q :=
+Fixpoint least_non_zero_coeff (p:Pol) : Coef :=
   match p with
     Pc c => c
-  | PX P i c => if Qzero_test c then least_non_zero_coeff P else c
+  | PX P i c => if czero_test c then least_non_zero_coeff P else c
   end.
 
-Inductive no_alternation : Pol1 Q -> Set :=
-  no_alternation_c1 : forall (c:Q) (q:Pol1 Q), q != Pc c -> no_alternation q
+Inductive no_alternation : Pol -> Set :=
+  no_alternation_c1 : forall c (q:Pol), q != Pc c -> no_alternation q
 | no_alternation_c2 :
-    forall n (a:Q) P1 P2,
+    forall n a P1 P2,
       no_alternation P2 -> a <> 0 ->
       0 <= a*(least_non_zero_coeff P2) ->
       P1 != (X^n *(Pc a+X*P2))%Pol -> no_alternation P1.
 
 
-Inductive one_alternation : Pol1 Q -> Set :=
+Inductive one_alternation : Pol -> Set :=
   one_alternation_here :
-    forall P (a:Q) (n:N) P1, (1 <= Z_of_N n)%Z ->
+    forall P a (n:N) P1, (1 <= Z_of_N n)%Z ->
       P != (X^n *(Pc a + X * P1))%Pol ->
       a * least_non_zero_coeff P1 < 0 -> no_alternation P1 ->
       one_alternation P
@@ -43,114 +44,178 @@ Inductive one_alternation : Pol1 Q -> Set :=
       one_alternation P1 -> 0 < a * least_non_zero_coeff P1 ->
       one_alternation P.
 
-Ltac clean_coefficients :=
-   unfold cops, Coef_record.Ceq, Coef_record.C1, 
-     Coef_record.C0, ceq_compat, 
-     Coef_record.Cadd, Coef_record.Cmul, Coef in *.
-
-Theorem Q0test_c : forall x:Q, Qzero_test x = true -> Qeq x 0.
-exact c0test_c.
-Qed.
-
-Theorem Qeq_propF : forall x y : Q, Qeq_bool x y = false -> ~x == y.
-exact ceq_propF.
-Qed.
-
-Theorem Q0test_Qeqb : forall x : Q, Qzero_test x = Qeq_bool x 0.
-exact c0test_ceqb.
-Qed.
-
-Hint Resolve Q0test_c.
-Hint Resolve c0test_c.
-Hint Unfold Coef_record.Ceq.
-Hint Immediate Qeq_sym.
+Hint Resolve c0test_c PolEq_refl ceq_refl.
+Hint Immediate ceq_sym PolEq_sym.
 
 Theorem least_non_zero_coeff_0 :
-   forall P, least_non_zero_coeff P == 0 -> P != P0.
+   forall P, least_non_zero_coeff P == c0 -> P != P0.
 induction P; simpl.
 intros; constructor;auto.
-caseEq (Qzero_test c).
+caseEq (czero_test c).
 intros Heqc Heq.
 constructor.
 auto.
 auto.
-intros Heq1 Heq2; elim (Qeq_propF c 0); auto.
-rewrite <- Q0test_Qeqb; trivial.
+
+intros Heq1 Heq2; elim (ceq_propF c 0); auto.
+rewrite <- c0test_ceqb; trivial.
 Qed.
 
 Lemma least_non_zero_coeff_P0_aux :
-  forall P P', P !=P' -> P0 = P' -> least_non_zero_coeff P == 0.
+  forall P P', P !=P' -> P0 = P' -> least_non_zero_coeff P == c0.
 intros P P' H; induction H.
 simpl; intros Heq; injection Heq.
 intros Heq'; rewrite Heq'; auto.
 intros Heq; discriminate.
 intros Heq; injection Heq; intros Heq'.
 simpl.
-caseEq (Qzero_test p).
+caseEq (czero_test p).
 simpl in * ; auto.
-intros Hcz; elim (Qeq_propF p 0).
-rewrite <- Q0test_Qeqb; auto.
+intros Hcz; elim (ceq_propF p c0).
+rewrite <- c0test_ceqb; auto.
 rewrite Heq'; auto.
 intros; discriminate.
 intros; discriminate.
 intros; discriminate.
 Qed.
 
-Lemma Q0test_morph :
-  forall p q, p == q -> Qzero_test p = Qzero_test q.
-intros p q H; caseEq (Qzero_test p).
+Lemma c0test_morph :
+  forall p q, p == q -> czero_test p = czero_test q.
+intros p q H; caseEq (czero_test p).
 intros Hp0; assert (ceq p 0).
 auto.
-caseEq (Qzero_test q); auto.
-intros Hqn0; elim (Qeq_propF q 0).
-rewrite <- Q0test_Qeqb; auto.
-apply Qeq_trans with p; auto.
-intros Hpn0; caseEq (Qzero_test q); auto.
-intros Hq0; elim (Qeq_propF p 0).
-rewrite <- Q0test_Qeqb; auto.
-apply Qeq_trans with q; auto.
+caseEq (czero_test q); auto.
+intros Hqn0; elim (ceq_propF q 0).
+rewrite <- c0test_ceqb; auto.
+apply ceq_trans with p; auto.
+intros Hpn0; caseEq (czero_test q); auto.
+intros Hq0; elim (ceq_propF p 0).
+rewrite <- c0test_ceqb; auto.
+apply ceq_trans with q; auto.
 Qed.
 
-Add Morphism (@Pc Q:Q->Pol) with signature Qeq ==> Pol_Eq as Pc_Morphism_Q.
-exact Pc_Morphism.
+Theorem czero_test_0 : czero_test c0 = true.
+rewrite c0test_ceqb.
+caseEq (ceqb c0 c0); trivial.
+intros habs; elim (ceq_propF c0 c0); auto.
 Qed.
 
-Add Morphism least_non_zero_coeff with signature Pol_Eq ==> Qeq
+Add Morphism least_non_zero_coeff with signature Pol_Eq ==> ceq
    as lnz_morphism.
 intros x1 x2 H; induction H; simpl;intros;auto.
-caseEq (Qzero_test q);auto.
+caseEq (czero_test q);auto.
 intros H';
-apply Qeq_trans with q; auto.
-apply Qeq_trans with 0; auto.
-caseEq (Qzero_test p);auto.
-intros H';apply Qeq_trans with 0; auto.
-apply Qeq_trans with p; auto.
-apply Qeq_sym; auto.
+apply ceq_trans with q; auto.
+apply ceq_trans with 0; auto.
+caseEq (czero_test p);auto.
+intros H';apply ceq_trans with 0; auto.
+apply ceq_trans with p; auto.
+apply ceq_sym; auto.
 
-  rewrite (Q0test_morph p q); auto.
-simpl; case (Qzero_test q); auto.
+rewrite (c0test_morph p q); auto.
+case (czero_test q); auto.
 simpl in IHPol_Eq.
-  rewrite (Q0test_morph p q); auto.
-simpl; case (Qzero_test q); auto.
+rewrite (c0test_morph p q); auto.
+case (czero_test q); auto.
 apply ceq_sym; auto.
 simpl in IHPol_Eq.
-  rewrite (Q0test_morph p q); auto.
-simpl; case (Qzero_test q); auto.
+rewrite czero_test_0 in IHPol_Eq.
+rewrite (c0test_morph p q); auto.
+case (czero_test q); auto.
 Qed.
 
 Add Morphism Qle with signature Qeq ==> Qeq ==> iff as Qle_morphism.
 Admitted.
 
-Add Morphism Pol_eval with signature Pol_Eq ==> Qeq ==> Qeq as Pol_eval_morphism.
+Add Morphism Pol_eval with signature Pol_Eq ==> ceq ==> ceq as Pol_eval_morphism.
 Admitted.
+
+Lemma PX_interp :
+   forall P n c, PX P n c != (X^(Npos n)*P+Pc c)%Pol.
+intros P n c.
+rewrite <- (mkPX_PX P n c c).
+setoid ring.
+simpl (X^(Npos n))%Pol.
+
+rewrite Pmul_PpowXP.
+unfold Pol_add, Pol_addC.
+apply mkPX_PX.
+setoid ring.
+Qed.
+
+Lemma Pol_mul_Rat_cst : forall a b, a !* Pc b != Pc (a**b).
+intros a b; unfold Pol_mul_Rat.
+caseEq (czero_test a).
+intros Hatest; setoid_replace a with c0.
+setoid_rewrite cmul_0_l; auto.
+auto.
+intros Hatest; caseEq (czero_test (a--c1)).
+intros Hatest1; assert (a**b==b).
+setoid_replace a with ((a --c1) ++ c1).
+setoid_rewrite (c0test_c (a--c1)); auto.
+setoid ring.
+setoid ring.
+setoid_rewrite H; auto.
+simpl.
+intros _; setoid_rewrite cmul_sym; auto.
+Qed.
+
+Lemma Pol_eval_mult_c :
+  forall c P x, Pol_eval (Pc c * P) x == c ** Pol_eval P x.
+intros c P x; induction P as [c' | P IHP i c'].
+simpl; setoid_rewrite Pol_mul_Rat_cst; simpl; setoid ring.
+simpl; setoid_rewrite mkPX_PX_c. 
+setoid_rewrite Pol_mul_Rat_cst; simpl.
+setoid_rewrite IHP; setoid ring.
+Qed.
 
 Lemma Pol_eval_plus :
-  forall P P' x, (Pol_eval (P + P')%Pol x == Pol_eval P x + Pol_eval P' x)%Q.
-Admitted.
+  forall P P' x, Pol_eval (P + P')%Pol x == Pol_eval P x ++ Pol_eval P' x.
+intros P; induction P; intros P' x; destruct P'.
+simpl; auto.
+simpl (Pc c + PX P' p c0).
+simpl; setoid ring.
+simpl; setoid ring.
+generalize (ZPminus_spec p p0).
+caseEq (ZPminus p p0).
+intros Hm Hpp0.
+simpl.
+rewrite Hm.
+rewrite Hpp0.
+setoid_rewrite mkPX_PX_c.
+simpl.
+setoid_rewrite IHP.
+setoid ring.
+setoid_rewrite (PX_interp P); setoid_rewrite (PX_interp P').
+simpl.
+setoid_Rewrite 
+Qed.
 
 Lemma Pol_eval_mult :
-  forall P P' x, Pol_eval (P * P')%Pol x == (Pol_eval P x * Pol_eval P' x)%Q.
+  forall P P' x, Pol_eval (P * P')%Pol x == (Pol_eval P x ** Pol_eval P' x).
+intros P; induction P as [c | P IHP i c]; intros P' x.
+setoid_rewrite Pol_eval_mult_c; simpl; auto.
+destruct P' as [c' | P' i' c'].
+setoid_rewrite Pmul_sym.
+setoid_rewrite Pol_eval_mult_c; simpl; setoid ring.
+simpl.
+setoid_rewrite mkPX_PX_c.
+Qed.
+simpl.
+
+
+simpl; auto.
+setoid_rewrite (Pol_mul_Rat_cst c' c).
+simpl.
+setoid ring.
+simpl.
+setoid_rewrite (Pol_mul_Rat_cst c' c).
+setoid_rewrite mkPX_PX_c.
+simpl.
+
+simpl; auto.
 Admitted.
+
 
 Lemma Pol_eval_pow :
   forall P n x, Pol_eval (P^n)%Pol x == ((Pol_eval P x)^nat_of_N n)%Q.
@@ -204,23 +269,9 @@ Lemma Qmult_1_r : forall x:Q, (x*1 == x)%Q.
 intros; ring.
 Qed.
 
-Lemma Pol_mul_Rat_cst : forall a b, a !* Pc b != Pc (a*b)%Q.
-Admitted.
 
 Add Morphism (@Pc Coef) with signature Qeq ==> Pol_Eq as Pc_morphism.
 Admitted.
-
-Lemma PX_interp :
-   forall P n (c:Q), PX P n c != (X^(Npos n)*P+Pc c)%Pol.
-intros P n c.
-rewrite <- (mkPX_PX P n c c).
-clean_coefficients; ring.
-simpl (X^(Npos n))%Pol.
-rewrite Pmul_PpowXP.
-unfold Pol_add, Pol_addC.
-apply mkPX_PX.
-setoid ring.
-Qed.
 
 
 Functional Scheme Npred_ind := Induction for Npred Sort Prop.
@@ -238,59 +289,284 @@ intros H; rewrite H; reflexivity.
 reflexivity.
 Qed.
 
+Theorem X_mul_convert_pow : forall p, X * p != X^1*p.
+intros; simpl;setoid ring.
+Qed.
+
 Lemma least_non_zero_P2 :
   forall P, exists n:N, exists P' : Pol,
    P != (X^n*(Pc(least_non_zero_coeff P)+X*P'))%Pol.
 intros P; induction P.
 exists 0%N; exists P0.
+
 simpl.
-rewrite Pol_mul_Rat_cst.
-apply Pc_Morphism.
-clean_coefficients.
-clean_coefficients; ring.
+unfold Pol_mul_Rat; rewrite czero_test_0.
+setoid ring.
 
 destruct IHP as [n [P' Heq]].
 simpl least_non_zero_coeff.
-caseEq (Qzero_test c).
+caseEq (czero_test c).
 intros Heqt; exists (n+Npos p)%N; exists P'.
 rewrite (PX_interp P p c).
-assert (Pc_c_0 : Pc c != P0).
-apply Pc_Morphism_Q; auto.
-rewrite Pc_c_0.
-rewrite Padd_0_r.
-
-rewrite Ppow_plus.
-rewrite (Pmul_sym(X^n)(X^Npos p)).
-rewrite <- Pmul_assoc.
-apply Pmul_Morphism.
+setoid_rewrite (c0test_c c); trivial.
+match goal with |- _ != ?x => pose (u:= x); fold u end.
+setoid_rewrite Heq; unfold u; rewrite Ppow_plus.
 setoid ring.
-assumption.
 
 intros _; exists 0%N; exists (X^(Npred (Npos p))*P)%Pol.
 simpl (X^0)%Pol.
-assert (Pc_1_P1 : Pc 1 != P1).
-apply PolEq_refl.
 rewrite (PX_interp P p c).
-assert (Pol_Pow1 : (forall a, X * a != X^1 *a)%Pol).
-simpl (X^1)%Pol.
-intros a.
-rewrite <- (PX_interp (Pc 1) 1 0).
-apply PolEq_refl.
-rewrite Pol_Pow1.
-rewrite Pmul_assoc.
+rewrite X_mul_convert_pow.
+setoid_replace (X^Npos p)%Pol with (X^1*X^Npred (Npos p))%Pol.
+setoid ring.
 rewrite <- Ppow_plus.
-replace (1+Npred(Npos p))%N with (Npos p).
-rewrite Padd_sym.
-rewrite Pmul_1_l.
-apply PolEq_refl.
-rewrite <- (Npred_correct p); trivial.
+rewrite <- (Npred_correct p); auto.
 Qed.
 
+Theorem least_non_zero_P3 : least_non_zero_coeff P0 == c0.
+apply ceq_refl.
+Qed.
+
+Theorem Pmul_0_l : forall x, P0 * x != P0.
+intros; setoid ring.
+Qed.
+
+Theorem Pc_eq_X_pow_mul_decompose :
+  forall Q a b n, Pc a != X^n*(Pc b + X*Q) -> a==b /\ (n = 0%N\/b==c0) /\ 
+              Q != P0.
+intros Q.
+induction Q; intros a b n.
+caseEq n.
+intros Hn0.
+subst n.
+caseEq (czero_test c).
+intros Hc0; setoid_rewrite (c0test_c c);auto.
+match goal with |- Pc a != ?X -> _ =>
+  setoid_replace X with (Pc b);[intros id; inversion id; auto | setoid ring]
+end.
+simpl (X^0); setoid ring.
+intros Hcn0.
+match goal with |- Pc a != ?X ->  _ =>
+  setoid_replace X with (PX (Pc c) 1 b);[intros id; inversion id | setoid ring]
+end.
+assert (H':Pc c != P0);[auto|inversion H'].
+rewrite c0test_ceqb in Hcn0.
+elim (ceq_propF c c0); auto.
+setoid_rewrite (PX_interp (Pc c)).
+ simpl Pol_pow; setoid ring.
+intros p Hn.
+
+match goal with |- Pc a != ?v -> _ =>
+    setoid_replace v with (PX (Pc b +X*Pc c) p c0)%Pol
+end.
+intros Heq; inversion Heq.
+assert (H' : Pc b+ c!*X != P0) by auto.
+unfold Pol_mul_Rat in H'.
+caseEq (czero_test c); intros Hctest; rewrite Hctest in H'; simpl in H'.
+inversion H'; assert (Hb: b++c0==c0) by assumption.
+setoid_rewrite cadd_0_r in Hb.
+setoid_rewrite Hb.
+assert (Hc : c == c0) by auto.
+setoid_rewrite Hc; auto.
+caseEq (czero_test (c--c1)).
+intros Hctest1; rewrite Hctest1 in H'.
+setoid_replace (Pc b + X) with (PX P1 1 b) in H'.
+inversion H'; elim P0_diff_P1;auto.
+rewrite (PX_interp P1); simpl Pol_pow; setoid ring.
+intros Hctest1; rewrite Hctest1 in H'.
+assert (Hctest' : czero_test (c1**c)=false).
+caseEq (czero_test (c1**c)); intros Hctest2; auto.
+elim (ceq_propF c 0).
+rewrite <- c0test_ceqb; auto.
+setoid_rewrite <- (c0test_c (c1**c)); auto.
+setoid ring.
+rewrite Hctest' in H'.
+simpl in H'.
+inversion H'; assert (Habsc : Pc (c1**c) != Pol_ring2.P0) by assumption.
+inversion Habsc.
+elim (ceq_propF (c1**c) c0); auto; rewrite <- c0test_ceqb;auto.
+setoid_rewrite (PX_interp (Pc b + X *Pc c)); setoid ring.
+caseEq n.
+intros Hn0; simpl (X^0);rewrite Pmul_1_l.
+simpl.
+rewrite Pscal_Pmul_l.
+caseEq (czero_test c).
+intros Hctest; setoid_rewrite (c0test_c _ Hctest); 
+setoid_rewrite Pmul_0_l; setoid_rewrite Padd_0_r.
+setoid_rewrite mkPX_PX_c; simpl.
+intros Heq; inversion Heq.
+assert (Hab : a==b).
+apply ceq_trans with (b++c0);[assumption|setoid ring].
+setoid_rewrite PX_interp.
+rewrite (Npred_correct p (Npos p)); auto.
+setoid_rewrite Ppow_plus.
+simpl (X^1).
+setoid_rewrite (Pmul_sym X).
+setoid_rewrite <- Pmul_assoc.
+setoid_replace (X*Q) with P0; [idtac | assumption].
+repeat (auto;fail || setoid ring || split).
+intros Hctest.
+setoid_rewrite mkPX_PX_c; setoid_rewrite (PX_interp (X*Q)).
+intros Heq.
+assert (H':Pc a != PX (PX Q p c) 1 b).
+setoid_rewrite (PX_interp (PX Q p c)).
+setoid_rewrite (PX_interp Q).
+simpl (X^1).
+apply PolEq_trans with (1:= Heq).
+setoid ring.
+inversion H'.
+assert (H'':PX Q p c != P0) by assumption.
+inversion H''.
+elim (ceq_propF c c0);auto.
+rewrite <- c0test_ceqb; auto.
+
+intros p' Hn; setoid_rewrite (PX_interp Q).
+match goal with |- _ != ?x -> _ =>
+   setoid_replace x with (PX (PX (PX Q p c) 1 b) p' c0)
+end.
+intros H'; inversion H'.
+assert (Ha:a == c0) by assumption; 
+assert (H'' : PX (PX Q p c) 1 b != P0) by assumption;
+clear - IHQ Ha H''.
+inversion H''.
+assert (Hb: b==c0) by assumption;
+assert (H'3: PX Q p c != P0) by assumption;
+clear - IHQ Ha Hb H'3.
+rewrite <- PX_interp.
+assert (a==b) by (apply ceq_trans with c0; auto).
+auto.
+setoid_rewrite (PX_interp (PX (PX Q p c) 1 b)).
+setoid_rewrite (PX_interp (PX Q p c)).
+setoid_rewrite (PX_interp Q).
+simpl (X^1); setoid ring.
+Qed.
+
+Theorem c0test_n : 
+   forall x, ~ x==c0 -> czero_test x = false.
+intros; caseEq (czero_test x); auto.
+intros H0; elim H; apply ceq_prop; rewrite <- c0test_ceqb; rewrite H0.
+exact I.
+Qed.
+
+Theorem least_non_zero_P4 :
+  forall P b n Q, P != X^n * (Pc b + X * Q) -> ~b==c0 ->
+            least_non_zero_coeff P == b.
+intros P; induction P.
+intros b n Q Heq Hbn0.
+destruct (Pc_eq_X_pow_mul_decompose _ _ _ _ Heq) as [Hc [Hn HQ]].
+exact Hc.
+
+intros b n Q.
+caseEq n.
+intros Hn0; simpl (X^0);
+match goal with |- _ != ?x -> _ => setoid_replace x with (PX Q 1 b) end.
+intros Heq Hbn0; inversion Heq.
+assert (Hcb : c==b) by assumption; clear - Hcb Hbn0.
+setoid_rewrite Hcb; simpl; rewrite (c0test_n b); auto.
+assert (Hcb : c==b) by auto; clear - Hcb Hbn0.
+setoid_rewrite Hcb; simpl; rewrite (c0test_n b); auto.
+assert (Hcb : c==b) by auto; clear - Hcb Hbn0.
+setoid_rewrite Hcb; simpl; rewrite (c0test_n b); auto.
+setoid_rewrite (PX_interp Q); simpl (X^1); setoid ring.
+
+intros p' Hn.
+match goal with |- _ != ?x -> _ => 
+  setoid_replace x with (PX (PX Q 1 b) p' c0)
+end.
+intros Heq Hbn0; inversion Heq.
+assert (Hc0 : c==c0) by assumption; assert (Hp :P !=PX Q 1 b) by assumption;
+clear - Hc0 Hbn0 Hp.
+assert (Hctest:czero_test c=true).
+setoid_rewrite Hc0; rewrite czero_test_0; trivial.
+simpl; rewrite Hctest.
+setoid_rewrite Hp; simpl.
+rewrite (c0test_n b); auto.
+assert (Hc0: c==c0) by auto; 
+ assert (Hp:P!=PX (PX Q 1 b) i c0) by assumption; clear - Hc0 Hbn0 IHP Hp.
+assert (Hctest:czero_test c=true).
+setoid_rewrite Hc0; rewrite czero_test_0; trivial.
+simpl; rewrite Hctest.
+setoid_rewrite Hp; simpl.
+rewrite czero_test_0.
+rewrite (c0test_n b); auto.
+assert (HPQ: PX Q 1 b != PX P i c0) by assumption; clear - HPQ Hbn0.
+inversion HPQ; elim Hbn0; auto.
+setoid_rewrite (PX_interp (PX Q 1 b)); setoid_rewrite (PX_interp Q).
+simpl (X^1); setoid ring.
+Qed.
+
+Theorem least_non_zero_P5 :
+  forall P b, P != Pc b -> least_non_zero_coeff P == b.
+intros P b Heq; setoid_rewrite Heq; auto.
+Qed.
+
+Theorem least_non_zero_P6 :
+  forall P n Q a, P != X^n * (Pc a + X * Q) -> a == c0 ->
+    least_non_zero_coeff P == least_non_zero_coeff Q.
+intros P; induction P; intros n Q a.
+intros Heq Ha.
+destruct (Pc_eq_X_pow_mul_decompose _ _ _ _ Heq) as [Hc [Hn HQ]].
+setoid_rewrite Hc; setoid_rewrite Ha; setoid_rewrite HQ; auto.
+intros Heq Ha.
+caseEq n.
+intros Hn0; rewrite Hn0 in Heq; simpl (X^0) in Heq.
+assert (H':PX P p c != PX Q 1 a).
+setoid_rewrite (PX_interp Q).
+simpl (X^1); setoid_rewrite Heq; setoid ring.
+assert (Hatest: czero_test a = true).
+setoid_rewrite Ha; apply czero_test_0.
+setoid_rewrite H'; simpl; rewrite Hatest; auto.
+intros p' Hn;
+assert (H':PX P p c != PX (PX Q 1 a) p' c0).
+rewrite Hn in Heq.
+setoid_rewrite Heq; setoid_rewrite (PX_interp (PX Q 1 a));
+setoid_rewrite (PX_interp Q).
+simpl (X^1);setoid ring.
+inversion H'.
+assert (Hc0:c==c0) by assumption; assert (HPQ : P!=PX Q 1 a) by assumption;
+clear - IHP Hc0 HPQ Ha.
+assert (Hctest: czero_test c = true).
+setoid_rewrite Hc0; apply czero_test_0.
+simpl; rewrite Hctest; apply IHP with (n:=0%N)(Q:=Q)(a:=a); trivial.
+apply PolEq_trans with (1:= HPQ); rewrite (PX_interp Q);
+simpl Pol_pow;setoid ring.
+assert (Hc0:c==c0) by auto; assert (HPQ: P!=PX(PX Q 1 a) i c0) by assumption;
+clear - IHP Hc0 HPQ Ha.
+simpl.
+assert (Hctest : czero_test c = true).
+setoid_rewrite Hc0; apply czero_test_0.
+rewrite Hctest.
+
+apply (IHP (Npos i) Q a); trivial.
+rewrite HPQ; setoid_rewrite (PX_interp (PX Q 1 a));
+setoid_rewrite (PX_interp Q); simpl (X^1);setoid ring.
+assert (Hc0 : c==c0) by assumption; 
+assert (HPQ : PX Q 1 a != PX P i c0) by assumption; 
+clear - IHP Hc0 HPQ.
+assert (Hctest : czero_test c = true).
+setoid_rewrite Hc0; apply czero_test_0.
+inversion HPQ.
+assert (H' : Q != P) by assumption; clear - Hc0 Hctest H'.
+setoid_rewrite H'; simpl.
+setoid_rewrite Hctest; auto.
+assert (H' : Q != PX P i0 c0) by assumption; clear - Hc0 Hctest H'.
+setoid_rewrite H'.
+simpl; rewrite Hctest;rewrite czero_test_0; auto.
+assert (H':P!=PX Q i0 c0) by assumption; clear - Hctest H'.
+setoid_rewrite H'; simpl; rewrite Hctest; rewrite czero_test_0; auto.
+Qed.
+
+Theorem least_non_zero_P7 :
+  forall b, least_non_zero_coeff (Pc b) == b.
+intros b; apply least_non_zero_P5; trivial.
+Qed.
 
 Theorem no_alternation_increasing :
   forall P, 0 <= least_non_zero_coeff P -> no_alternation P ->
   forall x y, 0 <= x <= y -> 0 <= Pol_eval P x <= Pol_eval P y.
-intros P H H1;generalize H;clear H.
+intTheorem least_non_zero_P3 :
+  forall P, Pnorm P = P0 -> least_non_zero_coeff P = 0.
+ros P H H1;generalize H;clear H.
 induction H1.
 intros H x y Hint.
 setoid_rewrite p.
@@ -312,6 +588,3 @@ rewrite Pol_eval_c.
 lazy beta iota zeta delta [least_non_zero_coeff] in Hpos;
 fold least_non_zero_coeff in Hpos. 
 
-
-
-*)
