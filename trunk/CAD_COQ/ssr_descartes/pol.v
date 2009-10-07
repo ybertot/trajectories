446ce19961b574f9f75b6149f6155b2447017d91
bertot <bertot@c63750b6-0a05-0410-b89b-bf6a48017dd0>
2009-10-07 14:21:22 +0000

    adds a few basic operations on polynomials: multiplication, multiplication by a scalar, translation, etc.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/cadcoq@104 c63750b6-0a05-0410-b89b-bf6a48017dd0

--- trunk/CAD_COQ/ssr_descartes/pol.v
+++ trunk/CAD_COQ/ssr_descartes/pol.v
@@ -1,3 +1,4 @@
+Require Import ssreflect.
 Require Import QArith List Omega ZArith seq xssralg infra.
 
 Import GRing.Theory .
@@ -10,3 +11,93 @@ Fixpoint eval_pol (l : seq Qcb)(x : Qcb) {struct l} : Qcb :=
   | a::tl => a + x * (eval_pol tl x)
   end.
 
+(* Polynomial are only represented by the list of their coefficients.
+  To any polynomial, we associate the absolute polynomial, whose
+  coefficients are the absolute values of the initial coefficients. *)
+
+Fixpoint abs_pol (l : seq Qcb) : seq Qcb :=
+ match l with nil => nil | a :: tl => (absr a) :: (abs_pol tl) end.
+
+(* The value of the absolute polynomial is always larger than the value
+ of the initial polynomial. *)
+
+Lemma ler_absr_eval_pol :
+  forall l x, absr (eval_pol l x) <<= eval_pol (abs_pol l) (absr x).
+Proof.
+elim => [|y s IHs] x /=; first by rewrite absr0.
+by apply: (ler_trans (absr_addr _ _)); rewrite lerTrb absr_mulr ler_lcompat ?absrpos.
+Qed.
+
+Lemma ler0_eval_pol_abs_pol :
+  forall l x, 0 <<= x -> 0 <<= eval_pol (abs_pol l) x.
+Proof.
+elim => [| y s Ihs] x hx /=; first by rewrite ler_refl.
+by apply: lerT0; rewrite ?absrpos // ler_0_lcompat // Ihs.
+Qed.
+
+Lemma eval_pol_abs_pol_increase : 
+  forall l x y, 0 <<= x -> x <<= y ->
+    eval_pol (abs_pol l) x <<= eval_pol (abs_pol l) y.
+elim=> [|u s Ihs] x y hx hy /=; first by rewrite ler_refl.
+rewrite lerTrb; apply ler_trans with (y * eval_pol (abs_pol s) x).
+  by rewrite ler_rcompat // ler0_eval_pol_abs_pol.
+by rewrite ler_lcompat // ?Ihs // (ler_trans hx).
+Qed.
+
+(* To describe polynomial addition, multiplication by a scalar, and
+  multiplication, we simply specify these operations so that their
+  interpretation through polynomial evaluation returns the corresponding
+  scalar operation. *)
+
+Definition add_pol :
+  forall l1 l2, {l' | forall x, eval_pol l1 x + eval_pol l2 x =
+  eval_pol l' x}.
+elim=> [|a l1 Ihl1] [|b l2].
+- by exists [::]; rewrite /= addr0.
+- by exists (b :: l2); move=> /= x; rewrite add0r.
+- by exists (a :: l1); move=> /= x; rewrite addr0.
+- (* ring needed!*)
+  case: (Ihl1 l2)=> [l' hl']; exists ((a + b) :: l'); move=> /= x.
+  rewrite [b + _]addrC addrA -[a + _ + _]addrA -mulr_addr hl'.
+  by rewrite -addrA [_ + b]addrC addrA.
+Defined.
+
+
+Definition s_mult_pol :
+  forall a l, {l' | forall x, a * eval_pol l x = eval_pol l' x}.
+move=> a; elim=> [| b l Ihl] /=.
+- by exists [::]; rewrite /= mulr0.
+- case: Ihl => l' hl'; exists ((a * b) :: l'); move=> /= x.
+  by rewrite -hl' mulr_addr 2!mulrA [x * _]mulrC.
+Defined.
+
+Definition mult_pol :
+  forall l1 l2, {l' | forall x, eval_pol l1 x * eval_pol l2 x =
+   eval_pol l' x}.
+elim=> [|a l1 Ihl1] /=.
+- by exists [::]; move=> /= x; rewrite mul0r.
+- move=> l2; case (s_mult_pol a l2) => l2a h.
+  case (Ihl1 (0 :: l2)) => l1l2x h1; case (add_pol l1l2x l2a)=> l' h2.
+  exists l'; move=> /= x; rewrite -h2 -h1 -h /= add0r mulr_addl.
+  by rewrite mulrA [_ * x]mulrC [a*_ + _]addrC.
+Defined.
+
+Definition mirror_pol :
+  forall l, {l' | forall x, eval_pol l (-x) = eval_pol l' x}.
+elim=> [|a l1 IHl1] /=.
+- by exists [::].
+case IHl1 => l' pl'.
+case (s_mult_pol (-1) l') => l'' pl''.
+by exists [:: a & l''] => x /=; rewrite -pl'' -pl' !mulNr mulrN mul1r.
+Defined.
+  
+Lemma translate_pol :
+  forall l a, {l' | forall x, eval_pol l x = eval_pol l' (x - a)}.
+elim=> [| b l Ihl]; move=> a /=.
+- by exists [::].
+- case (Ihl a) => l' h; case (mult_pol [:: a ; 1] l') => l2 h2.
+  case (add_pol [:: b] l2) => l3 h3.
+  exists l3; move=> x; rewrite -h3 -h2 h /= mulr0 !addr0 mulr1.
+  by rewrite (_ : a + (x - a) = x) // -oppr_sub oppr_add addrA
+       addrN add0r opprK.
+Qed.
