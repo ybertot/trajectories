46d838922c0404e26c990c008f56656b7e881b52
bertot <bertot@c63750b6-0a05-0410-b89b-bf6a48017dd0>
2009-10-12 20:20:30 +0000

    try some proofs about converting eval_pol to bigops
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/cadcoq@116 c63750b6-0a05-0410-b89b-bf6a48017dd0

--- trunk/CAD_COQ/ssr_descartes/pol.v
+++ trunk/CAD_COQ/ssr_descartes/pol.v
@@ -1,9 +1,14 @@
-Require Import ssreflect.
-Require Import QArith List Omega ZArith seq xssralg infra.
+Require Import QArith ZArith Zwf Omega.
+Require Import ssreflect eqtype ssrbool ssrnat div fintype seq ssrfun.
+Require Import bigops groups choice binomial.
+Require Export ssralg xssralg infra .
 
+Import GroupScope .
 Import GRing.Theory .
 Import GOrdered.
 Open Local Scope ring_scope .
+
+Set Printing Width 50.
 
 Fixpoint eval_pol (l : seq Qcb)(x : Qcb) {struct l} : Qcb :=
   match l with 
@@ -101,3 +106,67 @@ elim=> [| b l Ihl]; move=> a /=.
   by rewrite (_ : a + (x - a) = x) // -oppr_sub oppr_add addrA
        addrN add0r opprK.
 Qed.
+
+Lemma Qcb_Z : forall n : Z, Qred (Qmake n 1) == Qmake n 1.
+Proof.
+move=> n; apply/eqP; apply: Qcanon.Qred_identity => /=.
+rewrite Znumtheory.Zgcd_1_rel_prime.
+apply Znumtheory.rel_prime_sym.
+apply Znumtheory.rel_prime_1.
+Qed.
+
+Canonical Structure Qcb_make (n : Z) := QcbMake (Qcb_Z n).
+
+Lemma Qcb_make0 : Qcb_make 0 = 0.
+Proof. exact: val_inj. Qed.
+
+Lemma Qcb_make1 : Qcb_make 1 = 1.
+Proof. exact: val_inj. Qed.
+
+Definition Qbin m n := Qcb_make (Z_of_nat (bin m n)).
+
+Axiom QbinS : forall m n, Qbin m.+1 n.+1 = Qbin m n.+1 + Qbin m n.
+Axiom Qbin0 : forall m, Qbin m 0 = 1.
+
+Fixpoint qpower_rec (x :Qcb) (n:nat) :=
+   match n with 0%nat => 1 | S p => x * qpower_rec x p end.
+
+Definition qpower := nosimpl qpower_rec.
+
+Notation "x ^ y" := (qpower x y).
+
+Definition translate_pol' (l :seq Qcb) (a:Qcb) :=
+  mkseq (fun i:nat =>
+     \sum_(k < (size l).+1) Qbin k i * nth 0 l k * a ^ (k - i)) (size l).
+
+Lemma mkseq_shift :
+  forall T (f : nat ->T) n, mkseq f n.+1 = f 0%nat::mkseq (fun x => f x.+1) n.
+move => T f n; rewrite /mkseq -[n.+1]/(1+n)%nat iota_add addnC iota_addl /=.
+by congr (cons (f 0%nat)); rewrite -map_comp; apply: eq_in_map; move => x _ /=.
+Qed.
+
+Require Import String.
+Open Scope string_scope.
+Definition case_name (x:string) := True.
+
+Ltac Name_case s :=
+  match goal with
+     _ : case_name ?s' |- _ =>
+     let u:= eval compute in (s' ++ " not finished") in fail 1 u
+ | |- _ => assert (case_name s); [solve[exact I] | ]
+ end.
+
+Lemma eval_pol_big :
+  forall l x, eval_pol l x = \sum_(i < size l) nth 0 l i * x ^ i.
+move => l x; elim: l=> [ | a l IHl].
+Admitted.
+
+Lemma translate_pol'q :
+  forall l a x, eval_pol (translate_pol' l a) x = eval_pol l (x + a).
+move => l a; elim: l => [ | b l' IHl' x]; first by [].
+  have -> : eval_pol (b::l') (x+a) = b + (x + a) * eval_pol l' (x + a) by [].
+  rewrite /translate_pol'.
+  have -> : size (b::l') = (1+(size l'))%N by [].
+rewrite mkseq_shift.
+Admitted.
+
